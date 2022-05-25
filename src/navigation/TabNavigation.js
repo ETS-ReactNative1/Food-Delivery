94c48c0245e4447c3f9dd1c09fd3175e906df6af
Caner Al <caneral5858@gmail.com>
2022-05-25 16:11:24 +0300

    Bug fixed.

--- src/navigation/TabNavigation.js
+++ src/navigation/TabNavigation.js
@@ -1,10 +1,8 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {NavigationContainer} from '@react-navigation/native';
 import Home from '../screens/Home';
 import Profile from '../screens/Profile';
-import CustomDrawer from './CustomDrawer';
 
 const Tabs = createBottomTabNavigator();
 
